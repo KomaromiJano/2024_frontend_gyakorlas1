ed5ffa25a8ea852b47dbc7e0d1db03c920bafef3
Komáromi János <komaromijano@gmail.com>
2024-11-29 12:13:13 +0100

    feat/add grud and redesign peoples.components.html

--- src/app/peoples/peoples.component.ts
+++ src/app/peoples/peoples.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseService } from '../services/base.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-peoples',
@@ -10,24 +11,78 @@ export class PeoplesComponent {
 
   modal:boolean = false;
 
+  modal_type:string = "edit";
+
   peoples:any = []
 
-  edit_people:any = {}
+  temp_people:any = {}
 
   constructor(private base:BaseService){
-    this.base.getPeoples().valueChanges().subscribe(
-      (res)=> this.peoples=res
+    this.base.getPeoples().snapshotChanges().pipe(
+      map(
+        (changes)=>changes.map(
+          (c)=>({key:c.payload.key, ...c.payload.val()})
+        )
+      )
+    ).subscribe(
+      (res)=>this.peoples=res
     )
     this.modal = false;
   }
 
-  openModal(data:any){
+  openModal(data:any,type:string){
     this.modal = true;
-    this.edit_people = data;
+    this.modal_type = type;
+    this.temp_people = {...data};
   }
 
   closeModal(){
     this.modal = false;
+    this.temp_people = {}
+  }
+
+  create(){
+    this.base.addPeople(this.temp_people).subscribe(
+      () => {
+        console.log('A frissítése sikeres volt!');
+        this.closeModal()
+      },
+      (error) => {
+        console.error('Hiba a frissítéskor:', error);
+      }
+    )
+  }
+
+  update() {
+    const index = this.peoples.findIndex((people: any) => people.key === this.temp_people.key);
+  
+    if (index !== -1) {
+      // Az Observable használata esetén a subscribe() metódust kell hívni
+      this.base.updatePeople(this.temp_people.key, this.temp_people).subscribe(
+        () => {
+          console.log('A frissítése sikeres volt!');
+          this.closeModal()
+        },
+        (error) => {
+          console.error('Hiba a frissítéskor:', error);
+        }
+      );
+    }
+  }
+  
+  remove(){
+    const index = this.peoples.findIndex((people: any) => people.key === this.temp_people.key);
+    if(index !== -1){
+      this.base.deletePeople(this.temp_people.key).subscribe(
+        () => {
+          console.log('A törlés sikeres volt!');
+          this.closeModal();
+        },
+        (error) => {
+          console.error('Hiba a törléskor:', error);
+        }
+      )
+    }
   }
 
 }
--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,22 @@ export class BaseService {
 
   getPeoples() {
     return this.peoplesRef
+  }
+
+  // Egy ember adatainak frissítése  // Egy ember adatainak frissítése, Observable-t ad vissza
+  updatePeople(key: string, data: any): Observable<void> {
+    return from(this.peoplesRef.update(key, data));  // from() biztosítja, hogy Observable-t kapj
+  }
+
+  // Új ember hozzáadása
+  addPeople(data: any): Observable<any> {
+    return from(this.peoplesRef.push(data)).pipe();
+  }
+  
+
+  // Egy ember törlése
+  deletePeople(key: string): Observable<void> {
+    return from(this.peoplesRef.remove(key));
   }
 
 }
